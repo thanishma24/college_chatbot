32560842c947bd02b1b4d9c962d7c759f314954c
Thanishma <saithanishma24@gmail.com>
2025-02-28 11:17:45 +0530

    Update college.py

--- college.py
+++ college.py
@@ -12,7 +12,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Load CSV from GitHub (replace with your actual URL)
-csv_url = "svecw_details.csv"
+csv_url = "svcew_details.csv"
 
 try:
     df = pd.read_csv(csv_url)
